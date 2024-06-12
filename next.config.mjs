cd39d51becaa03277739fb683047f4b3ff64f74c
Alexander Angulo <alexanderangulo@Alexanders-MacBook-Pro.local>
2024-06-12 14:13:18 -0600

    wip()

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,21 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'ui-avatars.com',
-        port: '',
-        pathname: '/api/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
         port: '',
         pathname: '/u/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        port: '',
+        pathname: '/api/**',
       },
       {
         protocol: 'https',
