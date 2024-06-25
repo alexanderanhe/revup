72dbbc652ff1e0861ddc1a043472df40d6466687
Alexander Angulo <alexanderangulo@Alexanders-MacBook-Pro.local>
2024-06-25 17:24:37 -0600

    wip()

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -4,7 +4,7 @@ type CardProps = {
 }
 export default function Card({ className, children }: CardProps) {
   return (
-    <div className={`grid grid-cols-1 gap-4 items-center justify-start bg-base-100 shadow-2xl rounded-2xl w-full p-5 ${className ?? ''}`}>
+    <div className={`grid grid-cols-1 gap-1 items-center justify-start bg-base-100 shadow-2xl rounded-2xl w-full p-5 ${className ?? ''}`}>
         { children }
       </div>
   )
