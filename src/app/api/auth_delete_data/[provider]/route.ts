62b17124183e8fcc4f3f377329a020b9b9225218
Alexander Angulo <alexanderangulo@Alexanders-MacBook-Pro.local>
2024-06-25 17:25:35 -0600

    wip()

--- src/app/api/auth_delete_data/[provider]/route.ts
+++ src/app/api/auth_delete_data/[provider]/route.ts
@@ -1,8 +1,5 @@
-import { useRouter } from "next/router";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const router = useRouter()
-  console.log(router.query)
   return NextResponse.json({ status: "success" });
 }
