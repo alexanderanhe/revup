e46bb394e9e64d3017a37cdf778b7143d5c14d71
Alexander Angulo <alexanderangulo@Alexanders-MacBook-Pro.local>
2024-06-25 20:18:54 -0600

    wip()

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,7 +1,7 @@
 import LayoutContent from "@/components/templates/LayoutContent"
 import ThemeToggle from "@/app/ui/utils/ThemeToggle"
 import Card from "@/app/ui/Card"
-import { ArrowRightIcon, UserIcon } from "@heroicons/react/24/outline"
+import { ArrowLeftStartOnRectangleIcon, ArrowRightIcon, BellIcon, ChartBarSquareIcon, ChartPieIcon, Cog6ToothIcon, EnvelopeIcon, NewspaperIcon, ShieldCheckIcon, UserIcon } from "@heroicons/react/24/outline"
 
 const ProfilePage = () => {
   return (
@@ -37,17 +37,17 @@ const ProfilePage = () => {
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <NewspaperIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Achievement</span>
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <ChartPieIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Activity History</span>
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <ChartBarSquareIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Workout Progress</span>
             <ArrowRightIcon className="size-5" />
           </button>
@@ -57,7 +57,7 @@ const ProfilePage = () => {
         <Card>
           <h3 className="text-lg font-semibold">Notification</h3>
           <label className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <BellIcon className="size-5 text-primary" />
             <span className="label-text grow flex justify-start">Remember me</span>
             <input type="checkbox" className="toggle toggle-md toggle-secondary" defaultChecked />
           </label>
@@ -67,22 +67,22 @@ const ProfilePage = () => {
         <Card>
           <h3 className="text-lg font-semibold">Other</h3>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <EnvelopeIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Contact Us</span>
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <ShieldCheckIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Privacy Policy</span>
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <Cog6ToothIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Settings</span>
             <ArrowRightIcon className="size-5" />
           </button>
           <button className="btn btn-ghost w-full">
-            <UserIcon className="size-5 text-primary" />
+            <ArrowLeftStartOnRectangleIcon className="size-5 text-primary" />
             <span className="grow flex justify-start">Log out</span>
             <ArrowRightIcon className="size-5" />
           </button>
