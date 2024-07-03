3ba9bd323e7789fee51c309442315e6c72efb10b
Alexander Angulo <alexanderangulo@Alexanders-MacBook-Pro.local>
2024-07-03 17:21:53 -0600

    wip()

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,7 +2,7 @@ import { getPage } from '@/lib/notion';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+  if (req.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({error: 'Unauthorized'}, { status: 401 });
   }
   try {
