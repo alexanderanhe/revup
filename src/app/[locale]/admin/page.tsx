051a38c3706808cdc72fe987b42c98bbc17e2f2e
Alexander Angulo <alex.anguloh@gmail.com>
2024-07-11 11:00:55 -0600

    wip()

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,12 +1,5 @@
-import { getWorkoutsPage } from '@/lib/notion'
-import React from 'react'
-
 export default async function AdminPage() {
-  // const userList = await getNotionUserList();
-  // console.log(userList);
-  const page = await getWorkoutsPage();
-  console.log(page);
   return (
-    <pre>{JSON.stringify(page)}</pre>
+    <pre>HOLA</pre>
   )
 }
