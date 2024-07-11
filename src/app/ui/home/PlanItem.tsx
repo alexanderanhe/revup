0b34e29e1954f93e98a1d8d93c7df7835fee6eb8
Alexander Angulo <alex.anguloh@gmail.com>
2024-07-11 05:41:54 -0600

    wip()

--- src/app/ui/home/PlanItem.tsx
+++ src/app/ui/home/PlanItem.tsx
@@ -5,7 +5,7 @@ import ProgressCircle from "@/app/ui/utils/ProgressCircle";
 
 import { Plan } from "@/lib/definitions";
 import RatingStar from "../utils/RatingStar";
-import { HomeModernIcon } from "@heroicons/react/24/outline";
+import { HomeModernIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 type PlanItemProps = {
   plan: Plan;
   index: number;
@@ -21,10 +21,10 @@ export default async function PlanItem({ plan, index, t }: PlanItemProps) {
     <div className="collapse collapse-arrow join-item">
       <input type="radio" name="my-accordion-4" defaultChecked={!index} />
       <Card className="collapse-title">
-        <section className="grid grid-cols-[1fr_auto] max-sm:grid-cols-1 place-items-center w-full pr-4">
+        <section className="grid grid-cols-[1fr_auto] place-items-center w-full pr-4">
           <div className="grid grid-rows-auto gap-1 place-items-start w-full">
             <strong>{ plan.name }</strong>
-            <div className="flex gap-2 text-xs max-sm:gap-1 max-sm:text-[0.6rem] uppercase">
+            <div className="flex gap-2 text-xs max-sm:gap-1 uppercase">
               <HomeModernIcon className="size-3 text-primary" />
               <span>{ place }</span>
               <RatingStar
@@ -35,26 +35,30 @@ export default async function PlanItem({ plan, index, t }: PlanItemProps) {
               />
               <span>{ difficulty}</span>
             </div>
-            <span className="text-xs">
-              { t("planDetails", { days: plan.days, sets: plan.sets_per_week }) }
-            </span>
+            <span className="text-xs">{ t("planDetailsDays", { days: plan.days }) }</span>
+            <span className="text-xs">({ t("planDetailsSets", { sets: plan.sets_per_week }) })</span>
           </div>
-          <div className="max-sm:hidden">
-            <ProgressCircle progress={Math.round(Math.random() * 100)} />
-          </div>
+          <ProgressCircle progress={Math.round(Math.random() * 100)} />
         </section>
       </Card>
-      <div className="collapse-content max-h-44 space-y-2 py-2">
+      <div className="collapse-content h-[50svh] overflow-y-auto space-y-1 px-0 py-2">
         <h4>{ t("nextTraining") }</h4>
-        <ul className="grid grid-cols-1 w-full">
+        <ul className="grid grid-cols-1 gap-2 w-full">
           { Array.from({ length: plan.days }).map((_, index) => (
-            <Link
-            key={`day${index}`}
-              href={`/workout?day=${index + 1}`}
-              className="join join-vertical h-12 border-base-300 border p-3"
-            >
-              { index + 1 }. Day { plan.body_zones?.[index % plan.body_zones.length] ?? 'PLAN'}
-            </Link>
+            <Card key={`day${index}`} className="w-full">
+              <Link
+                href={`/workout?day=${index + 1}`}
+                className="flex gap-3 w-full"
+              >
+                <div className="flex flex-col justify-center font-medium w-full">
+                  <span className="text-xs">{ t("planDetailsDay", { day: index + 1 }) }</span>
+                  <span className="font-semibold [&::first-letter]:uppercase">
+                    { plan.body_zones?.[index % plan.body_zones.length] ?? '-'}
+                  </span>
+                </div>
+                <ProgressCircle progress={0} type="error" icon={<LockClosedIcon className="size-5" />} />
+              </Link>
+            </Card>
           ))}
         </ul>
       </div>
--- src/app/ui/utils/ProgressCircle.tsx
+++ src/app/ui/utils/ProgressCircle.tsx
@@ -1,17 +1,29 @@
 'use client'
 
+import clsx from "clsx";
+
 type ProgressCircleProps = {
   progress: number;
+  type?: 'success' | 'error' | 'warning' | 'info';
+  icon?: React.ReactNode;
 }
 
-export default function ProgressCircle({ progress }: ProgressCircleProps) {
+export default function ProgressCircle({ progress, type, icon }: ProgressCircleProps) {
   const workoutProgressStyles = {
     "--value": progress,
     "--size": "3.2rem"
   } as React.CSSProperties;
   return (
-    <div className="radial-progress text-sm font-semibold before:text-success" style={workoutProgressStyles} role="progressbar">
-      { progress }{ '%' }
+    <div className={clsx(
+      "radial-progress bg-success/10 text-sm font-semibold",
+      (!type || type === 'success') && "bg-success/10 before:text-success",
+      type === 'success' && "bg-success/10 before:text-success",
+      type === 'error' && "bg-error/10 before:text-error",
+      type === 'warning' && "bg-warning/10 before:text-warning",
+      type === 'info' && "bg-info/10 before:text-info",
+      !progress && 'before:hidden after:hidden',
+    )} style={workoutProgressStyles} role="progressbar">
+      { icon ?? `${progress}%` }
     </div>
   )
 }
