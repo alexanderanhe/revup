2085ef3c16f094d75f0488679031c983bbe74bfc
Alexander Angulo <alex.anguloh@gmail.com>
2024-07-20 00:46:19 -0600

    wip()

--- src/app/ui/exercises/HistoryTable.tsx
+++ src/app/ui/exercises/HistoryTable.tsx
@@ -1,30 +1,51 @@
 import { getWorkoutItems } from "@/lib/data";
+import { WorkoutComplex } from "@/lib/definitions";
+import { Fragment } from "react";
 
 type HistoryTableProps = {
   workout_id: string;
 }
+type HistoryGroup = {
+  [key: string]: (WorkoutComplex & { ts_time: string })[]
+}
 
 export default async function HistoryTable({ workout_id }: HistoryTableProps) {
-  const history = await getWorkoutItems(workout_id);
+  const histories = await getWorkoutItems(workout_id);
+  const grouped = histories?.reduce((acc, { created_at, ...rest }) => {
+    const key = created_at?.toLocaleDateString();
+    if (!key) {
+      return acc;
+    }
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push({
+      ...rest,
+      ts_time: created_at?.toLocaleTimeString() ?? ''
+    });
+    return acc;
+  }, {} as HistoryGroup);
   return (
     <div className="overflow-x-auto">
       <table className="table table-xs">
-        { history?.map(({ reps, weight, weight_unit, time, time_unit, created_at }, index) => (
-          <>
+        { grouped && Object.entries(grouped).map(([ts_date, history]) => (
+          <Fragment key={`history${ts_date}`}>
             <thead className="bg-neutral text-neutral-content">
               <tr>
-                <th colSpan={3}>{ created_at?.toLocaleString() }</th>
+                <th colSpan={3}>{ ts_date }</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <th>{ index + 1 }</th>
-                <td>{ !reps ? `${time} ${time_unit}` : `${weight} ${weight_unit} x ${reps} reps` }</td>
-                <td>{ `${created_at?.getHours().toString().padEnd(2, "0")}:${created_at?.getMinutes().toString().padEnd(2, "0")}` }</td>
-              </tr>
-            </tbody>
-          </>
-        )) }
+            { history?.map(({ reps, weight, weight_unit, time, time_unit, ts_time }, index) => (
+              <tbody key={`history${ts_date}_${ts_time}${index}`}>
+                <tr>
+                  <th>{ index + 1 }</th>
+                  <td>{ !reps ? `${time} ${time_unit}` : `${weight} ${weight_unit} x ${reps} reps` }</td>
+                  <td width={90}>{ ts_time }</td>
+                </tr>
+              </tbody>
+            ))}
+          </Fragment>
+        ))}
       </table>
     </div>
   )
