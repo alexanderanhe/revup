056ea260eb29329b66b35f8afe88d78b037afd25
Alexander Angulo <alex.anguloh@gmail.com>
2024-07-29 14:48:16 -0600

    wip()

--- src/app/ui/home/dashboard/Recommendations.tsx
+++ src/app/ui/home/dashboard/Recommendations.tsx
@@ -4,7 +4,7 @@ export default function Recommendations({ translate }: { translate?: {[key: stri
   return (
     <div className="w-full overflow-y-auto space-y-1 py-2">
       <div className="font-semibold">{ translate?.title ?? "-" }</div>
-      <div className="w-full inline-flex flex-nowrap">
+      {/* <div className="w-full inline-flex flex-nowrap">
         <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
           { Array.from({length: 26}, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i)).map((letter) => (
             <li key={letter} className="flex-none">
@@ -12,7 +12,7 @@ export default function Recommendations({ translate }: { translate?: {[key: stri
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
       <p>{ translate?.noRecommendations }</p>
     </div>
   )
