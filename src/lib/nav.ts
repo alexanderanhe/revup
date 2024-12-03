428fd413665d6a225485744c6e9dbe49849722e5
Alexander Angulo <alex.anguloh@gmail.com>
2024-12-03 14:50:03 -0600

    update: remove unused service worker development file and comment out social navigation link

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -8,7 +8,7 @@ const menuNavLinks: MenuNavLinks[] = [
   { name: 'Assessment', href: '/assessment', current: ['nav'] },
   { name: 'Ejercicios', href: '/workout', current: ['nav'] },
   { name: 'Explore', href: '/explore', current: ['footer'], Icon: Rocket, animate: "animate-bounce" },
-  { name: 'Social', href: '/social', current: ['nav', 'footer'], Icon: SquarePlayIcon },
+  // { name: 'Social', href: '/social', current: ['nav', 'footer'], Icon: SquarePlayIcon },
   { name: 'Workout', href: '/exercises', current: ['footer'], Icon: Dumbbell },
   // { name: 'Logs', href: '/logs', current: ['nav', 'footer'], Icon: TrendingUp, animate: "animate-bounce" },
   { name: 'Perfil', href: '/profile', current: ['footer'], Icon: UserRoundCog, animate: "animate-bounce" },
